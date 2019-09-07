4f1a4bdf62cb71876eaa8b4078238ae74e69942e
Saar Yogev <saaryogev@gmail.com>
2019-09-07 23:08:08 +0300

    Added expense model

---
+++ app/src/main/java/com/saaryogev/financeapp/Expense.kt
@@ -0,0 +1,5 @@
+package com.saaryogev.financeapp
+
+import java.util.*
+
+data class Expense(private val amount: Number, private val type: String, private val paymentMethod: String, private val date: Date)
