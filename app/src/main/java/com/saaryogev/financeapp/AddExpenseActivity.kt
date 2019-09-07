92c2d9ae363f4df4590c1e05e151d1d68abceaeb
Saar Yogev <saaryogev@gmail.com>
2019-09-07 22:45:09 +0300

    Added empty saveExpenseForm function and linked it to the layout

--- app/src/main/java/com/saaryogev/financeapp/AddExpenseActivity.kt
+++ app/src/main/java/com/saaryogev/financeapp/AddExpenseActivity.kt
@@ -2,11 +2,16 @@ package com.saaryogev.financeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AddExpenseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_expense)
+    }
+
+    fun saveExpenseForm(view: View) {
+
     }
 }
