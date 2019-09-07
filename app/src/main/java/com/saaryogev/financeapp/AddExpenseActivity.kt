4df27a08b8f5ee1a6b37bcd0f8e19abf6ad0d938
Saar Yogev <saaryogev@gmail.com>
2019-09-07 23:08:30 +0300

    implemented saveExpenseForm function

--- app/src/main/java/com/saaryogev/financeapp/AddExpenseActivity.kt
+++ app/src/main/java/com/saaryogev/financeapp/AddExpenseActivity.kt
@@ -3,8 +3,14 @@ package com.saaryogev.financeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import java.util.*
 
 class AddExpenseActivity : AppCompatActivity() {
+
+    private val amountText = findViewById<EditText>(R.id.amountText)
+    private val paymentTypeText = findViewById<EditText>(R.id.paymentTypeText)
+    private val paymentMethodText = findViewById<EditText>(R.id.paymentMethodText)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +18,6 @@ class AddExpenseActivity : AppCompatActivity() {
     }
 
     fun saveExpenseForm(view: View) {
-
+        val expense = Expense(amountText.text.toString().toDouble(), paymentTypeText.text.toString(), paymentMethodText.text.toString(), Date())
     }
 }
